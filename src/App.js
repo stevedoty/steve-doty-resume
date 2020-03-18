2f95a11a93dce5d0c4d9a13a26ac08e8d308d22c
Steve Doty <stevedoty16@gmail.com>
2020-03-18 14:13:37 -0700

    replace create-react-app boilerplate with todo app source code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            list: [
+                {"title":"title1","detail":"detail1"},
+                {"title":"title2","detail":"detail2"},
+                {"title":"title3","detail":"detail3"},
+            ],
+            title: "stateTitle",
+            detail: "stateDetail"
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        console.log("yo")
+    }
+
+    render(){
+    console.log(this.state.list)
+    const displayList = this.state.list.map( (listItem) =>
+        <li>{listItem.title} == {listItem.detail}</li>
+    )
+
+        return (
+            <div>
+                <header>
+                    <form className="addNewItem">
+                        <label>
+                            title:
+                            <input type="text"
+                            />
+                        <label>
+                        </label>
+                            detail:
+                            <input type="text"/>
+                        </label>
+                        <button
+                        onClick={this.handleClick}
+                        />
+                    </form>
+                </header>
+                <br></br>
+                {displayList}
+            </div>
+        )
+    }
 }
 
 export default App;
